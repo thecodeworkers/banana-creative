e54d759911d63e1d6f52500c6c2e6694175f4b73
MauriceMiot <maurice.yggdrasil@gmail.com>
2021-03-16 16:15:33 -0400

    all featured migrate and fixed navbar

---
+++ src/screens/FeaturedTwo/interface.ts
@@ -0,0 +1,17 @@
+export type propsType = {
+	imageDescription: string,
+	date: string,
+	keywords: string,
+	title: string,
+	subtitle?: string,
+	number: string,
+	description: string,
+	descriptionTwo: string,
+	descriptionThree: string,
+	descriptionFour: string,
+	image: any,
+	action: any,
+	theme: boolean,
+	transition?: boolean
+	id?: string
+}
