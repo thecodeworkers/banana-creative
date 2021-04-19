11fc2a0ffa341527cedf528bbd12de9e340d1197
gibercode <giberlara@gmail.com>
2021-04-19 14:34:59 -0400

    graphql configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import { Loader, Home } from '../components'
+import { useSelector } from 'react-redux'
+import { wrapper } from '@store'
+import { getPages } from '@store/actions'
+
+const HomePage = () => {
+
+ const state = useSelector(state => state) 
+
+ console.log(state)
+
+  return (
+    <Loader>
+      <Home />
+    </Loader>
+  )
+}
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  ({ store }) => store.dispatch(getPages('homePage'))
+)
+
+
+export default HomePage
