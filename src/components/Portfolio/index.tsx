aaada337a8b3456272225f41717e4fae3885161f
gibercode <giberlara@gmail.com>
2021-04-23 19:06:58 -0400

    grid

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Welcome } from '@components';
+import { Welcome, NavBar, Menu  } from '@components';
 import { Grid } from './elements'
 
 const Portfolio = ({ content }) => {
 
   return (
     <div>
+      <NavBar />
+      <Menu />
       <Welcome title='About us' component={false} />
       <Grid />
     </div>
